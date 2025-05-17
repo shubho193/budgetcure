181a2ab6e949040da53d9b80f313556fa16128e7
shubho193 <prawper193@gmail.com>
2025-05-18 01:37:10 +0530

    build fix

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,5 +1,9 @@
-const fs = require('fs-extra');
-const path = require('path');
+import fs from 'fs-extra';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define source and destination directories
 const srcDir = path.join(__dirname, '../public/assets');
